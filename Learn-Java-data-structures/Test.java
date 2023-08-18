c1739ccf3a9374139b5ed96d30d6908ece3e4d5b
jagadeeshwaran7 <jagadeeshwaranr@jerusalemengg.ac.in>
2023-08-19 02:54:30 +0530

    Data Structure: added some methods to array

---
+++ Learn-Java-data-structures/Test.java
@@ -0,0 +1,8 @@
+import array.ArrayProblem;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(ArrayProblem.getIndex(arr, 5));
+    }
+}
