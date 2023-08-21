ee06730e1087ecbf8ca0acccb1169372556f6e12
jagadeeshwaran7 <jagadeeshwaranr@jerusalemengg.ac.in>
2023-08-21 06:48:06 +0530

    Algorithms: added first methods to Searching class

---
+++ Learn-Java-data-structures/searching/Searching.java
@@ -0,0 +1,10 @@
+package searching;
+
+public class Searching {
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) return i;
+        }
+        return -1;
+    }
+}
