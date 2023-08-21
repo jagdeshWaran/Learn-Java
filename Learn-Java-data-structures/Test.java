41ad1d6aff4d81172cc2d59bbc71cc812771faea
jagadeesh waran <142624143+jagdeshWaran@users.noreply.github.com>
2023-08-21 07:11:11 +0530

    Delete Learn-Java-data-structures directory
    unwanted packages

--- Learn-Java-data-structures/Test.java
+++
@@ -1,8 +0,0 @@
-import array.ArrayProblem;
-
-public class Test {
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        System.out.println(ArrayProblem.getIndex(arr, 5));
-    }
-}
