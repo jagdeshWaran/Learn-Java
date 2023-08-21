bf2f28fb4df78b9d62104c7a2b35cbcf6c56d34d
jagadeeshwaran7 <jagadeeshwaranr@jerusalemengg.ac.in>
2023-08-21 06:18:40 +0530

    Data Structure: added some methods to array

--- Learn-Java-data-structures/array/ArrayProblem.java
+++ Learn-Java-data-structures/array/ArrayProblem.java
@@ -1,10 +1,74 @@
 package array;
 
 public class ArrayProblem {
-    public static int getIndex(int[] arr, int find) {
+
+    private ArrayProblem() {
+
+    }
+
+    public static int getIndex(int[] arr, int find) throws IndexOutOfBoundsException {
         for (int index = 0; index <= arr.length; index++) {
             if (find == arr[index]) return index;
         }
         return -1;
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (target < arr[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int binarySearch(int[] arr, int left, int right, int target) {
+        if (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == target) return mid;
+            else if (arr[mid] < target) {
+                return binarySearch(arr, mid + 1, right, target);
+            } else {
+                return binarySearch(arr, left, mid - 1, target);
+            }
+        }
+        return -1;
+    }
+
+    public static int[] bubbleSort(int[] arr) {
+        int size = arr.length;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - 1; j++) {
+                if (arr[i] < arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static int[] insertionSort(int[] arr) {
+
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+        return arr;
     }
 }
